e912ccd6f5ccbe4bf7b3c9e35dda74200252b195
Di Truong Ly <ditruongly@gmail.com>
2019-11-19 23:52:47 +0100

    another way to register objects of the model for use in the django admin site

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
